90ae22d2638c353f5e4276cc8f9bdf5f4d683b90
Kieran Harwood <KH951@student.aru.ac.uk>
2025-02-19 12:42:18 +0000

    Fuel bar v2 bult to run basic funtion and ready to add more upgrades to said system.

--- Assets/Assigemnt alterations/scripts/Fuelbar.cs
+++ Assets/Assigemnt alterations/scripts/Fuelbar.cs
@@ -7,15 +7,20 @@ public class Fuelbar : MonoBehaviour
 {
 
     public Slider slider;
+    public Gradient gradient;
+    public Image fill;
 
     public void SetMaxFuel(int Fuel)
     {
         slider.maxValue = Fuel;
         slider.value = Fuel;
+        fill.color = gradient.Evaluate(1f);
     }
     public void SetHealth(int Fuel)
     {
         slider.value = Fuel;
+
+        fill.color = gradient.Evaluate(slider.normalizedValue);
     }
        
     
